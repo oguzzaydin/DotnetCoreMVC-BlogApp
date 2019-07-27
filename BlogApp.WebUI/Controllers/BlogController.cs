788ae4fd87e706c6396757c49c99aaae0508a6f9
oguzzaydin <oguzhanaydin56@gmail.com>
2019-07-27 21:46:26 +0300

    Blog and Category list

---
+++ BlogApp.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IBlogRepository blogRepository;
+
+        public BlogController(IBlogRepository _blogRepository)
+        {
+            blogRepository = _blogRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View(blogRepository.GetAll());
+        }
+    }
+}
---
+++ BlogApp.WebUI/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BlogApp.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoryController(ICategoryRepository _categoryRepository)
+        {
+            categoryRepository = _categoryRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View(categoryRepository.GetAll());
+        }
+    }
+}
